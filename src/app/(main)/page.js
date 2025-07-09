abfa9f49a778c68ea69a99e34da4f0737d72a713
Joe <joantpat@gmail.com>
2025-07-09 03:56:40 +0100

    Add default redirect

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className="flex grow">
-      Content coming soon :)
-    </div>
-  );
+export default async function Home({params}) {
+  redirect('/about');
 }
